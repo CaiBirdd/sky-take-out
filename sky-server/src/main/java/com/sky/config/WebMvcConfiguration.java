d011a7bc1902f550bc59150ca10ead8a57f15693
CaiBirdd <2960693496@qq.com>
2026-08-25 17:20:29 +0800

    feat:day7添加购物车完成，修复拦截器的bug

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -31,25 +31,21 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     @Autowired
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
+    @Autowired
+    private JwtTokenUserInterceptor jwtTokenUserInterceptor;
+
     /**
      * pc管理端注册自定义拦截器
      *
      * @param registry
      */
+    @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin/employee/login");
-    }
-    /**
-     * 微信小程序端自定义拦截器，不拦截登录和商铺状态接口
-     * @param registry
-     */
-    @Autowired
-    JwtTokenUserInterceptor jwtTokenUserInterceptor;
-    protected void addInterceptors1(InterceptorRegistry registry) {
-        log.info("开始注册自定义拦截器...");
+
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
