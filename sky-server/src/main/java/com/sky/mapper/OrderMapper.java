ce831158978e7ac98955d82189212b805675b02d
CaiBirdd <2960693496@qq.com>
2026-08-27 18:39:43 +0800

    feat:day9 admin端统计订单状态的数量完成

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -42,4 +42,12 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id = #{id}")
     Orders getById(Long id);
+
+    /**
+     * 统计订单状态数量
+     * @param status
+     * @return
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
 }
